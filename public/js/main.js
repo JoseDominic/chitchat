1caf74fb75e7de815703cf22851ad2eac2b9a9ea
josedominic <josudominic007@gmail.com>
2020-03-27 17:41:24 +0530

    finished messaging server side

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,22 @@
 const socket = io();
+
+//catch messages to socket and console log
+
+socket.on('message',message => {
+    console.log(message);
+});
+
+//get users message from chat form
+
+const chatForm = document.getElementById('chat-form');
+
+chatForm.addEventListener('submit', (e) => {
+    //prevent the form from submitting to a page
+    e.preventDefault();
+
+    //get the user message from the form i/p element msg
+    const msg = e.target.elements.msg.value;
+    
+    //send the user message to server
+    socket.emit('userMessage',msg);
+});
--- server.js
+++ server.js
@@ -13,7 +13,24 @@ app.use(express.static(path.join(__dirname,'public')));
 //listen for a web socket connection event and take action
 io.on('connection',socket => {
     console.log(`New web socket connection...`);
-})
+
+    //Welcome message for new user
+    socket.emit('message','Welcome to ChitChat!');
+
+    //send message to all client sockets when a user joins the chat
+    //broadcast will be send to all users except the new user
+    socket.broadcast.emit('message','A user has joined the chat');
+
+    //notify all other users on disconnection
+    socket.on('disconnect',() => {
+        io.emit('message','A user has left the chat');
+    });
+
+    //catch user textMessage
+    socket.on('userMessage',msg => {
+        io.emit('message',msg);
+    });
+});
 
 const PORT = process.env.PORT || 5000;
 
